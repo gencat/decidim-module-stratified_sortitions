33751ad6e4b1265a7691f67918eafa011a2189a9
Ruben <ruben.bg@coditramuntana.com>
2026-01-12 12:15:53 +0100

    Add frozen string literal to migration

--- db/migrate/20260109105643_allow_null_value_in_substrata.rb
+++ db/migrate/20260109105643_allow_null_value_in_substrata.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AllowNullValueInSubstrata < ActiveRecord::Migration[7.0]
   def change
     change_column_null :decidim_stratified_sortitions_substrata, :value, true
