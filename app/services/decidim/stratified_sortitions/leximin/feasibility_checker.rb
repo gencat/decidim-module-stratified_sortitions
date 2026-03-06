eb1f2bbe6d58ac769845ae99a884b3d63047c0db
Ruben <ruben.bg@coditramuntana.com>
2026-03-06 13:44:57 +0100

    Fix lints

--- app/services/decidim/stratified_sortitions/leximin/feasibility_checker.rb
+++ app/services/decidim/stratified_sortitions/leximin/feasibility_checker.rb
@@ -163,38 +163,47 @@ module Decidim
           strata = @cb.strata_info
           return errors if strata.size < 2
 
-          # For each stratum, check that the sum of volunteers per substratum
-          # that also appear in at least one substratum of every other stratum
-          # is enough to fill the panel
           strata.each do |stratum|
-            stratum_name = extract_name(stratum[:name])
-
-            stratum[:substrata].each do |substratum|
-              cat_id = substratum[:id]
-              max_quota = substratum[:max_quota]
-              percentage = substratum[:percentage]
-              volunteers_in_cat = @cb.category_volunteers[cat_id]&.size || 0
-
-              # Skip if no restriction (percentage 0 means unrestricted), no quota, or no volunteers
-              next if percentage.zero? || max_quota.zero? || volunteers_in_cat.zero?
-
-              # Check if this substratum's max quota exceeds available volunteers
-              next unless max_quota > volunteers_in_cat
-
-              substratum_name = extract_name(substratum[:name])
-
-              errors << I18n.t(
-                "decidim.stratified_sortitions.errors.feasibility.substratum_quota_exceeds_volunteers",
-                substratum_name:,
-                stratum_name:,
-                max_quota:,
-                volunteers_count: volunteers_in_cat,
-                percentage:
-              )
-            end
+            errors.concat(validate_stratum_quotas(stratum))
           end
 
           errors
+        end
+
+        def validate_stratum_quotas(stratum)
+          errors = []
+          stratum_name = extract_name(stratum[:name])
+
+          stratum[:substrata].each do |substratum|
+            error = validate_substratum_quota(substratum, stratum_name)
+            errors << error if error.present?
+          end
+
+          errors
+        end
+
+        def validate_substratum_quota(substratum, stratum_name)
+          cat_id = substratum[:id]
+          max_quota = substratum[:max_quota]
+          percentage = substratum[:percentage]
+          volunteers_in_cat = @cb.category_volunteers[cat_id]&.size || 0
+
+          # Skip if no restriction (percentage 0 means unrestricted), no quota, or no volunteers
+          return nil if percentage.zero? || max_quota.zero? || volunteers_in_cat.zero?
+
+          # Check if this substratum's max quota exceeds available volunteers
+          return nil unless max_quota > volunteers_in_cat
+
+          substratum_name = extract_name(substratum[:name])
+
+          I18n.t(
+            "decidim.stratified_sortitions.errors.feasibility.substratum_quota_exceeds_volunteers",
+            substratum_name:,
+            stratum_name:,
+            max_quota:,
+            volunteers_count: volunteers_in_cat,
+            percentage:
+          )
         end
 
         def extract_name(name_field)
